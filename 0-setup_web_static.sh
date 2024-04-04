8d2dedf1a34ae1b4eb5c3755bc5823324e61636a
Meshack Bwire <b.meshack@outlook.com>
2024-04-04 06:03:08 -0700

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,40 @@
 #!/usr/bin/env bash
 # Installs, configures, and starts the web server
 SERVER_CONFIG="server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-	server_name _;
-	index index.html index.htm;
-	error_page 404 /404.html;
-	add_header X-Served-By \$hostname;
+    server_name _;
+    index index.html index.htm;
+    error_page 404 /404.html;
+    add_header X-Served-By \$hostname;
 
-	location / {
-		root /var/www/html/;
-		try_files \$uri \$uri/ =404;
-	}
+    location / {
+        root /var/www/html/;
+        try_files \$uri \$uri/ =404;
+    }
 
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		try_files \$uri \$uri/ =404;
-	}
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        try_files \$uri \$uri/ =404;
+    }
 
-	if (\$request_filename ~ redirect_me) {
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
+    if (\$request_filename ~ redirect_me) {
+        rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+    }
 
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-	}
+    location = /404.html {
+        root /var/www/error/;
+        internal;
+    }
 }"
-HOME_PAGE="<!DOCTYPE html>
-<html lang='en-US'>
-	<head>
-		<title>Home - AirBnB Clone</title>
-	</head>
-	<body>
-		<h1>Welcome to AirBnB!</h1>
-	<body>
-</html>
-"
+HOME_PAGE="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
 # shellcheck disable=SC2230
 if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
     apt-get update
@@ -56,7 +53,7 @@ chown -hR ubuntu:ubuntu /data
 bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
 ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 if [ "$(pgrep -c nginx)" -le 0 ]; then
-	service nginx start
+    service nginx start
 else
-	service nginx restart
+    service nginx restart
 fi
