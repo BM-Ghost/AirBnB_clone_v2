e1a60495e5e409fb14d9977ed02dce0c54b0a790
Meshack Bwire <b.meshack@outlook.com>
2024-04-23 05:19:14 -0700

    states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,17 +1,22 @@
-cript starts Flask web app
-    listen on 0.0.0.0, port 5000
-    routes: /:                    display "Hello HBNB!"
-            /hbnb:                display "HBNB"
-            /c/<text>:            display "C" + text (replace "_" with " ")
-            /python/<text>:       display "Python" + text (default="is cool")
-            /number/<n>:          display "n is a number" only if int
-            /number_template/<n>: display HTML page only if n is int
-            /number_odd_or_even/<n>: display HTML page; display odd/even info
-            /states_list:         display HTML and state info from storage;
+#!/usr/bin/python3
 """
+Script starts Flask web app
+listen on 0.0.0.0, port 5000
+routes:
+    /:                        display "Hello HBNB!"
+    /hbnb:                    display "HBNB"
+    /c/<text>:                display "C" + text (replace "_" with " ")
+    /python/<text>:           display "Python" + text (default="is cool")
+    /number/<n>:              display "n is a number" only if int
+    /number_template/<n>:     display HTML page only if n is int
+    /number_odd_or_even/<n>:  display HTML page; display odd/even info
+    /states_list:             display HTML and state info from storage;
+"""
+
 from models import storage
 from models import *
 from flask import Flask, render_template
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -88,3 +93,4 @@ def html_fetch_states():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
